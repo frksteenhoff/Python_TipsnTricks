e9758f2bac23831f043c0c3011be4d2368c0b6a9
Henriette Steenhoff <henriette.steenhoff@gmail.com>
2017-02-19 13:48:56 +0100

    Added extra explanation of functionality

--- UrlReadPandas.py
+++ UrlReadPandas.py
@@ -1,15 +1,17 @@
-# Reading csv file from url into pandas dataframe
-# Reading csv file from local path to pandas dataframe
+# Goal of this file, teaching you how to:
+# - read .csv-file from url into pandas dataframe
+# - read .csv-file from local path to pandas dataframe
 
 import io
 import requests
 import pandas as pd
 
-# url - the path to you data
+# This URL is from the Stanford Statweb, - Datasets for "The Elements of Statistical Learning"
 url    = "https://statweb.stanford.edu/~tibs/ElemStatLearn/datasets/SAheart.data"
 source = requests.get(url).content
 data   = pd.read_csv(io.StringIO(source.decode('utf-8')))
 
-# Reading the data into pandas dataframe from local folder
+
+# Reading the data into pandas dataframe from local folder, this is the same dataset as the one above
 data = pd.read_csv('data/SAheart.csv')
 data
